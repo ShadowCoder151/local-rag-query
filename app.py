c74b26ecd754e182781308a8dc6d1dc1cd8c6792
ShadowCoder151 <shadowcoder023@gmail.com>
2025-07-10 21:39:02 +0530

    feat(app.py): Update app.py with embedder + vec store block

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from ingest.loader import PDFLoad
 from ingest.chunker import DocChunk
 from embed.embedder import Embedder
+from embed.vec_store import VecStore
 from pprint import pprint
 
 res = PDFLoad("data/test_doc.pdf")
@@ -12,5 +13,18 @@ chk = DocChunk(res)
 embedder = Embedder()
 embd = embedder.embed_docs(chk)
 
+dim = embd[0][0].shape[0]
+store = VecStore(dim=dim)
+store.add_embed(embd)
+store.save()
+
+store.load()
+query = "What is an large language model?"
+q_vec = embedder.embed_query(query)
+res = store.search(q_vec, k=3)
+
+
+
 pprint(embd[0])
 print(len(chk), len(embd))
+pprint(res)
