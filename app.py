b4b518a862d348a42fe10f0b9b2d630581232aa4
ShadowCoder151 <shadowcoder023@gmail.com>
2025-07-11 21:25:32 +0530

    update(app.py): Basic RAG pipeline completed with addition of Retriever class + LLM inference class

--- app.py
+++ app.py
@@ -3,28 +3,49 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 from ingest.loader import PDFLoad
 from ingest.chunker import DocChunk
+
 from embed.embedder import Embedder
 from embed.vec_store import VecStore
+
+from rag.retriever import Retriever
+from rag.prompt_build import PromptBuilder
+
+from backend.llm_backend import LLM
+
 from pprint import pprint
 
-res = PDFLoad("data/test_doc.pdf")
-chk = DocChunk(res)
+# res = PDFLoad("data/test_doc.pdf")
+# chk = DocChunk(res)
 
 embedder = Embedder()
-embd = embedder.embed_docs(chk)
+# embd = embedder.embed_docs(chk)
 
-dim = embd[0][0].shape[0]
-store = VecStore(dim=dim)
-store.add_embed(embd)
-store.save()
+# dim = embd[0][0].shape[0]
+# store = VecStore(dim=dim)
+# store.add_embed(embd)
+# store.save()
 
+
+store = VecStore(dim=384)
 store.load()
-query = "What is an large language model?"
-q_vec = embedder.embed_query(query)
-res = store.search(q_vec, k=3)
+
+
+query = "What is safety in pretraining?"
+ret = Retriever(embedder, store, 3)
+res = ret.retrieve(query)
+
+build = PromptBuilder()
+prompt = build.build_prompt(query, res)
+
+llm = LLM()
+response = llm.generate(prompt)
+
+# q_vec = embedder.embed_query(query)
+# res = store.search(q_vec, k=3)
 
 
 
 # pprint(embd[0])
 # print(len(chk), len(embd))
-pprint(sorted(res, key=lambda x:x["score"]))
+# pprint(sorted(res, key=lambda x:x["score"]))
+print(f"Response: {response}")
